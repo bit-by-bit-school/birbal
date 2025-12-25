aca53fbd3ad25d956315d2e26fc3ac26f46520ed
AkshayGuptaK <akshaygrkesh@gmail.com>
2025-12-25 23:41:23 +0530

    Add search for similar docs to query

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from dotenv import load_dotenv
 from parse_org_roam import org_files_to_dataframes
 from embedding import embed_df
+from query import query_vector
+
+def load():
+    df = org_files_to_dataframes()
+    embed_df(df)
 
 def main():
     load_dotenv()
-    df = org_files_to_dataframes()
-    embed_df(df)
+    print(query_vector("What are examples of search sort correspondence?"))
+
 
 
 if __name__ == "__main__":
---
+++ query.py
@@ -0,0 +1,28 @@
+# This module takes a user query and fetches the relevant context and provides it to the LLM
+
+import os
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+
+def query_vector(query_str):
+    persist_directory = os.getenv('PERSIST_DIR')
+    embedding_model = os.getenv('EMBEDDING_MODEL')
+    embeddings = OllamaEmbeddings(model=embedding_model)
+    
+    vectordb = Chroma(collection_name="org_roam", embedding_function=embeddings, persist_directory=persist_directory)
+    retriever = vectordb.as_retriever(search_type="similarity", search_kwargs={"k": 3}) # parametrize
+    retrieved_docs = retriever.invoke(query_str)
+
+    return retrieved_docs
+
+
+# # Retrieve docs
+# retrieved_docs = vectordb.similarity_search_with_score(query_str, k=15)
+# org_link_format = "[%.2f]: [[id:%s][%s]] \n %s"
+# docs = [org_link_format % (score, doc.metadata["ID"],
+#                            doc.metadata["title"].strip(),
+#                                doc.metadata["hierarchy"].strip())
+#                          for doc, score in retrieved_docs]
+
+
+
