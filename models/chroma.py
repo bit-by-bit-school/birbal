3320b1e0174778a25af7677fe8d9737b3e419343
AkshayGuptaK <akshaygrkesh@gmail.com>
2025-12-27 23:31:27 +0530

    Refactor: Add ChromaStore class

---
+++ models/chroma.py
@@ -0,0 +1,24 @@
+from langchain_chroma import Chroma
+from config import config
+
+class ChromaStore:
+    def __init__(self, embeddings):
+        self.conn = Chroma(
+            collection_name=config["collection_name"],
+            embedding_function=embeddings,
+            persist_directory=config["persist_dir"],
+        )
+
+    def add_texts(self, texts, metadatas, ids):
+        self.conn.add_texts(texts, metadatas=metadatas, ids=ids)
+
+    def similarity_search(self, query_str):
+        retriever = self.conn.as_retriever(
+            search_type="similarity",
+            search_kwargs={"k": config["k_nearest_neighbors_to_retrieve"]},
+        )
+        return retriever.invoke(query_str)
+
+    def filter_by_metadata(self, metadata_field, metadata_value):
+        raw = self.conn.get(where={metadata_field: metadata_value})
+        return raw["documents"]
--- query.py
+++ query.py
@@ -1,39 +1,22 @@
 # This module queries the vector db and provides the desired document contents
 
 from langchain_ollama import OllamaEmbeddings
-from langchain_chroma import Chroma
+from models import chroma
 from config import config
 
 
 def query_vector(query_str):
     embeddings = OllamaEmbeddings(model=config["embedding_model"])
-    vectordb = Chroma(
-        collection_name=config["collection_name"],
-        embedding_function=embeddings,
-        persist_directory=config["persist_dir"],
-    )
-    retriever = vectordb.as_retriever(
-        search_type="similarity",
-        search_kwargs={"k": config["k_nearest_neighbors_to_retrieve"]},
-    )
-    retrieved_docs = retriever.invoke(query_str)
+    vectordb = chroma.ChromaStore(embeddings)
+    return vectordb.similarity_search(query_str)
 
-    return retrieved_docs
 
 def query_by_id(root_id: str):
     """Return all documents who lie in the subtree rooted at `root_id`."""
-    lower = id_prefix
-    upper = id_prefix + ".\uffff"
-
     embeddings = OllamaEmbeddings(model=config["embedding_model"])
-    vectordb = Chroma(
-        collection_name=config["collection_name"],
-        embedding_function=embeddings,
-        persist_directory=config["persist_dir"],
-    )
+    vectordb = chroma.ChromaStore(embeddings)
+    return vectordb.filter_by_metadata(metadata_field="root_id", metadata_value=root_id)
 
-    raw = vectordb.get(where={"root_id": root_id})
-    return raw["documents"]
 
 # # Retrieve docs
 # retrieved_docs = vectordb.similarity_search_with_score(query_str, k=15)
