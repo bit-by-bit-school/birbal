13958c6d4d9f7b342eb76e00dc4fc9a1bbe0092b
AkshayGuptaK <akshaygrkesh@gmail.com>
2026-01-14 14:18:27 +0530

    Replace langchain with base Ollama client

--- birbal/llm.py
+++ birbal/llm.py
@@ -1,34 +1,31 @@
 # This module takes a user query and generates a RAG context-fuelled response
-from langchain.agents import create_agent
-from langchain.agents.middleware import dynamic_prompt, ModelRequest
 from birbal.ai import get_ai_provider
 from birbal.query import query_vector
 
 
-@dynamic_prompt
-def prompt_with_context(request: ModelRequest) -> str:
-    """Inject context into state messages."""
-    last_query = request.state["messages"][-1].text
-    retrieved_docs = query_vector(last_query)
-
-    docs_content = "\n\n".join(retrieved_docs)
-
-    print(docs_content, flush=True)
-    system_message = (
-        "You are a retrieval-augmented assistant. Retrieve only, no guessing. If the answer is not in the context, say you don't know."
-        "Cite the source context files used at the end."
-        f"\n\n{docs_content}"
-    ) # need structured prompting?
-
-    return system_message
-
-
 def query_llm(query):
-    llm = get_ai_provider().get_llm()
-    agent = create_agent(llm, tools=[], middleware=[prompt_with_context])
-    for chunk, metadata in agent.stream(
-        {"messages": [{"role": "user", "content": query}]},
-        stream_mode="messages",
-    ):
-        if hasattr(chunk, "content"):
-            yield chunk.content
+    retrieved_docs = query_vector(query)
+    docs_content = "\n\n".join(retrieved_docs)
+    print(docs_content, flush=True)
+
+    system_prompt = f"""
+        You are a research assistant. Answer the user's question using ONLY the provided context. 
+        If the answer is not in the context, state that you do not know.
+
+        <context>
+        {docs_content}
+        </context>
+
+        At the end of your response, provide a section titled "SOURCES" listing only the sources used to answer the question.
+        """
+    
+    llm = get_ai_provider().get_llm(stream=True)
+
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": query},
+    ]
+
+    for chunk in llm.invoke(messages):
+        if "message" in chunk and "content" in chunk["message"]:
+            yield chunk["message"]["content"]
--- birbal/models/ollama.py
+++ birbal/models/ollama.py
@@ -1,5 +1,4 @@
 from ollama import Client
-from langchain_ollama import ChatOllama
 from birbal.models.base import AIProvider
 from birbal.config import config
 
@@ -10,9 +9,6 @@ class OllamaEmbeddings:
 
     dimensions: int | None = None
     """Dimensions of generated vector embedding"""
-
-    _client: Client | None = None
-    """The client to use for making requests."""
 
     mirostat: int | None = None
     """Enable Mirostat sampling for controlling perplexity.
@@ -79,66 +75,266 @@ class OllamaEmbeddings:
     to more diverse text, while a lower value (e.g., `0.5`) will
     generate more focused and conservative text. (Default: `0.9`)"""
 
-    def __init__(self, model, dimensions):
+    def __init__(self, host, model, dimensions, **kwargs):
+        self.client = Client(host=host)
         self.model = model
         self.dimensions = dimensions
-        self._set_client()
-
-    @property
-    def _default_params(self):
-        """Get the default parameters for calling Ollama."""
-        return {
-            "mirostat": self.mirostat,
-            "mirostat_eta": self.mirostat_eta,
-            "mirostat_tau": self.mirostat_tau,
-            "num_ctx": self.num_ctx,
-            "num_gpu": self.num_gpu,
-            "num_thread": self.num_thread,
-            "repeat_last_n": self.repeat_last_n,
-            "repeat_penalty": self.repeat_penalty,
-            "temperature": self.temperature,
-            "stop": self.stop,
-            "tfs_z": self.tfs_z,
-            "top_k": self.top_k,
-            "top_p": self.top_p,
-        }
-
-    def _set_client(self):
-        """Set client to use for Ollama."""
-        self._client = Client(host=config["ollama_host"])
-        return self
+        self.options = kwargs
 
     def embed_documents(self, texts: list[str]) -> list[list[float]]:
-        """Embed search docs."""
-        if not self._client:
+        if not self.client:
             msg = (
                 "Ollama client is not initialized. "
                 "Please ensure Ollama is running and the model is loaded."
             )
             raise ValueError(msg)
-        return self._client.embed(
-            self.model,
-            texts,
-            options=self._default_params,
-            keep_alive=self.keep_alive,
+
+        response = self.client.embed(
+            model=self.model,
+            input=texts,
             dimensions=self.dimensions,
-        )["embeddings"]
+            options=self.options,
+        )
+
+        return response["embeddings"]
 
     def embed_query(self, text: str) -> list[float]:
-        """Embed query text."""
         return self.embed_documents([text])[0]
+
+
+class OllamaLLM:
+    model: str
+    """Model name to use."""
+
+    reasoning: bool | str | None = None
+    """Controls the reasoning/thinking mode for [supported models](https://ollama.com/search?c=thinking).
+
+    - `True`: Enables reasoning mode. The model's reasoning process will be
+        captured and returned separately in the `additional_kwargs` of the
+        response message, under `reasoning_content`. The main response
+        content will not include the reasoning tags.
+    - `False`: Disables reasoning mode. The model will not perform any reasoning,
+        and the response will not include any reasoning content.
+    - `None` (Default): The model will use its default reasoning behavior. Note
+        however, if the model's default behavior *is* to perform reasoning, think tags
+        (`<think>` and `</think>`) will be present within the main response content
+        unless you set `reasoning` to `True`.
+    - `str`: e.g. `'low'`, `'medium'`, `'high'`. Enables reasoning with a custom
+        intensity level. Currently, this is only supported `gpt-oss`. See the
+        [Ollama docs](https://github.com/ollama/ollama-python/blob/da79e987f0ac0a4986bf396f043b36ef840370bc/ollama/_types.py#L210)
+        for more information.
+    """
+
+    validate_model_on_init: bool = False
+    """Whether to validate the model exists in Ollama locally on initialization.
+
+    !!! version-added "Added in `langchain-ollama` 0.3.4"
+    """
+
+    mirostat: int | None = None
+    """Enable Mirostat sampling for controlling perplexity.
+
+    (Default: `0`, `0` = disabled, `1` = Mirostat, `2` = Mirostat 2.0)
+    """
+
+    mirostat_eta: float | None = None
+    """Influences how quickly the algorithm responds to feedback from generated text.
+
+    A lower learning rate will result in slower adjustments, while a higher learning
+    rate will make the algorithm more responsive.
+
+    (Default: `0.1`)
+    """
+
+    mirostat_tau: float | None = None
+    """Controls the balance between coherence and diversity of the output.
+
+    A lower value will result in more focused and coherent text.
+
+    (Default: `5.0`)
+    """
+
+    num_ctx: int | None = None
+    """Sets the size of the context window used to generate the next token.
+
+    (Default: `2048`)
+    """
+
+    num_gpu: int | None = None
+    """The number of GPUs to use.
+
+    On macOS it defaults to `1` to enable metal support, `0` to disable.
+    """
+
+    num_thread: int | None = None
+    """Sets the number of threads to use during computation.
+
+    By default, Ollama will detect this for optimal performance. It is recommended to
+    set this value to the number of physical CPU cores your system has (as opposed to
+    the logical number of cores).
+    """
+
+    num_predict: int | None = None
+    """Maximum number of tokens to predict when generating text.
+
+    (Default: `128`, `-1` = infinite generation, `-2` = fill context)
+    """
+
+    repeat_last_n: int | None = None
+    """Sets how far back for the model to look back to prevent repetition.
+
+    (Default: `64`, `0` = disabled, `-1` = `num_ctx`)
+    """
+
+    repeat_penalty: float | None = None
+    """Sets how strongly to penalize repetitions.
+
+    A higher value (e.g., `1.5`) will penalize repetitions more strongly, while a
+    lower value (e.g., `0.9`) will be more lenient. (Default: `1.1`)
+    """
+
+    temperature: float | None = None
+    """The temperature of the model.
+
+    Increasing the temperature will make the model answer more creatively.
+
+    (Default: `0.8`)
+    """
+
+    seed: int | None = None
+    """Sets the random number seed to use for generation.
+
+    Setting this to a specific number will make the model generate the same text for the
+    same prompt.
+    """
+
+    stop: list[str] | None = None
+    """Sets the stop tokens to use."""
+
+    tfs_z: float | None = None
+    """Tail free sampling.
+
+    Used to reduce the impact of less probable tokens from the output.
+
+    A higher value (e.g., `2.0`) will reduce the impact more, while a value of `1.0`
+    disables this setting.
+
+    (Default: `1`)
+    """
+
+    top_k: int | None = None
+    """Reduces the probability of generating nonsense.
+
+    A higher value (e.g. `100`) will give more diverse answers, while a lower value
+    (e.g. `10`) will be more conservative.
+
+    (Default: `40`)
+    """
+
+    top_p: float | None = None
+    """Works together with top-k.
+
+    A higher value (e.g., `0.95`) will lead to more diverse text, while a lower value
+    (e.g., `0.5`) will generate more focused and conservative text.
+
+    (Default: `0.9`)
+    """
+
+    keep_alive: int | str | None = None
+    """How long the model will stay loaded into memory."""
+
+    base_url: str | None = None
+    """Base url the model is hosted under.
+
+    If none, defaults to the Ollama client default.
+
+    Supports `userinfo` auth in the format `http://username:password@localhost:11434`.
+    Useful if your Ollama server is behind a proxy.
+
+    !!! warning
+        `userinfo` is not secure and should only be used for local testing or
+        in secure environments. Avoid using it in production or over unsecured
+        networks.
+
+    !!! note
+        If using `userinfo`, ensure that the Ollama server is configured to
+        accept and validate these credentials.
+
+    !!! note
+        `userinfo` headers are passed to both sync and async clients.
+
+    """
+
+    client_kwargs: dict | None = {}
+    """Additional kwargs to pass to the httpx clients. Pass headers in here.
+
+    These arguments are passed to both synchronous and async clients.
+
+    Use `sync_client_kwargs` and `async_client_kwargs` to pass different arguments
+    to synchronous and asynchronous clients.
+    """
+
+    async_client_kwargs: dict | None = {}
+    """Additional kwargs to merge with `client_kwargs` before passing to httpx client.
+
+    These are clients unique to the async client; for shared args use `client_kwargs`.
+
+    For a full list of the params, see the [httpx documentation](https://www.python-httpx.org/api/#asyncclient).
+    """
+
+    sync_client_kwargs: dict | None = {}
+    """Additional kwargs to merge with `client_kwargs` before passing to httpx client.
+
+    These are clients unique to the sync client; for shared args use `client_kwargs`.
+
+    For a full list of the params, see the [httpx documentation](https://www.python-httpx.org/api/#client).
+    """
+
+    def __init__(self, host, model, stream, **kwargs):
+        self.client = Client(host=host)
+        self.model = model
+        self.stream = stream
+        self.options = kwargs
+
+    def bind(self, **kwargs):
+        settings = {**self.options, **kwargs}
+        return OllamaLLM(
+            model=self.model, 
+            host=str(self.client._client.base_url), 
+            stream=self.stream, 
+            **settings
+        )
+
+    def invoke(self, messages: list[dict]):
+        """
+        Note: Native Ollama uses [{'role': 'user', 'content': '...'}] format.
+        """
+        response = self.client.chat(
+            model=self.model,
+            messages=messages,
+            stream=self.stream,
+            options=self.options,
+        )
+
+        if self.stream:
+            return response
+
+        return response["message"]["content"]
 
 
 class OllamaProvider(AIProvider):
     def get_embedder(self):
         return OllamaEmbeddings(
-            model=config["embedding_model"], dimensions=config["vector_dims"]
+            host=config["ollama_host"],
+            model=config["embedding_model"],
+            dimensions=config["vector_dims"],
         )
 
     def get_llm(self, stream=True):
-        return ChatOllama(
+        return OllamaLLM(
+            host=config["ollama_host"],
             model=config["large_language_model"],
+            stream=stream,
             num_ctx=config["context_window_size"],
             temperature=0,
-            stream=stream,
         )
